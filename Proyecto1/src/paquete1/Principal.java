3b650e8f098f765d6e32639d4166d70a301f1dbc
martarobina <martarobina@A120-PROFE>
2024-03-05 13:38:45 +0100

    He añadido un comentario de prueba

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Control de versiones de ejemplo");
 		System.out.println("Segundo mensaje");
+		// Comentario para probar remoto
 	}
 
 }
